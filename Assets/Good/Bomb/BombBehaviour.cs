2fdde966871bb5358887cc194d6ec00723c41f29
Konstantin Barkalov <mail@barkalov.ru>
2023-07-10 01:59:08 +0700

    bomb destroy fix

--- Assets/Good/Bomb/BombBehaviour.cs
+++ Assets/Good/Bomb/BombBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BombBehaviour : MetaManagerSourceBehaviour
 {
     public ExtendedPhysics.Explosion.ExplosionBehaviour explosionPrefab;
+    public float force = 300;
     public BeaconBehaviour bombBeacon;
     public float exlpodeTime = 2f;
     public float alarmDuration = 60f/180f * 2f;
@@ -21,8 +22,9 @@ public class BombBehaviour : MetaManagerSourceBehaviour
             isAlarmed = true;
             bombBeacon.mode = BeaconMode.Flash;
         }
-        if (Time.fixedTime > exlpodeTime ) {
-            Instantiate<ExtendedPhysics.Explosion.ExplosionBehaviour>(explosionPrefab, transform.position, transform.rotation);
+        if (Time.fixedTime > exlpodeTime) {
+            var explosion = Instantiate<ExtendedPhysics.Explosion.ExplosionBehaviour>(explosionPrefab, transform.position, transform.rotation);
+            explosion.explosionSphere.force=force;
             Destroy(this.gameObject);
         }
     }
